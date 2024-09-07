439628ee40bee6c453e8982901711964b8b8304c
Amber Nguyen <ambernguyen@Ambers-MBP-2.lan>
2024-09-06 20:12:54 -0400

    changed iframe to an image for resume

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
+import Image from 'next/image'; // Assuming you're using Next.js Image component
 
 export default function ResumePage() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to detect screen width and set whether the device is mobile
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768); // Set breakpoint at 768px
+  };
+
+  useEffect(() => {
+    // Check on initial load
+    handleResize();
+
+    // Add event listener to detect window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div style={{ backgroundColor: "#1C3A5F" }}>
       <Navbar />
@@ -10,17 +30,26 @@ export default function ResumePage() {
           <h1 style={{ color: "white", fontSize: "4rem", fontWeight: "bold" }}>My Resume</h1>
         </div>
         <div className="col-12" style={{ textAlign: "center" }}>
-          <iframe
-            src="https://portfolio-images-1.s3.us-east-2.amazonaws.com/AmberNguyen_Resume.pdf"
-            style={{
-              width: '70%',
-              height: '700px',
-              border: '5px solid white', // Thin white border
-              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', // Subtle shadow
-              borderRadius: '8px', // Rounded corners for a softer look
-            }}
-            title="Resume"
-            className="resume-iframe"
+          {/* Download Link */}
+          <p style={{ fontFamily: "Times New Roman", color: "white", fontSize: "1.5rem" }}>
+            Download my resume{" "}
+            <a 
+              href="https://portfolio-images-1.s3.us-east-2.amazonaws.com/AmberNguyen_Resume.pdf" 
+              style={{ textDecoration: "underline", color: "#5A8FBD" }} 
+              target="_blank"
+              download
+            >
+              here
+            </a>
+            !
+          </p>
+          <Image 
+            src="https://portfolio-images-1.s3.us-east-2.amazonaws.com/AmberNguyen_resume.png"
+            className="img-fluid"
+            width={700} // Adjust width for the image
+            height={1000} // Maintain aspect ratio
+            id="resume-image"
+            alt="Resume"
           />
         </div>
       </div>
